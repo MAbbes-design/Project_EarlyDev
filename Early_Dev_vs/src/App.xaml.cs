e35a12ebd807c7a445d9cd68c1403f9b78d8e978
Mohamed <L00188528@atu.ie>
2025-05-02 18:17:47 +0100

    Creating thre folder structure for our .net Maui/C# project. Added additional folders for the following - Unit testing in the next delivery date. - git workflows for DEVOPS CA2 - infrastructure for deployment scripts - separate source folder to organise the files better.

---
+++ Early_Dev_vs/src/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Early_Dev_vs
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
